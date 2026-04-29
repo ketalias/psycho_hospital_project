ca59ff283a0d828d2f7d4ee15e9db577bdd88988
Roma <159444348+ketalias@users.noreply.github.com>
2026-04-29 08:26:03 +0300

    feat: implement CRUD views for appointments with cookie-based diagnosis filtering and session tracking

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,4 @@
+import urllib.parse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -12,28 +13,35 @@ class AppointmentListView(ListView):
 
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
-        # Збереження фільтру в cookies
         if 'diagnosis' in request.GET:
-            response.set_cookie('last_diagnosis_filter', request.GET.get('diagnosis'), max_age=60*60*24*30)
+            encoded_val = urllib.parse.quote(request.GET.get('diagnosis'))
+            response.set_cookie('last_diagnosis_filter', encoded_val, max_age=60*60*24*30)
         return response
 
     def get_queryset(self):
         queryset = super().get_queryset()
         diagnosis_query = self.request.GET.get('diagnosis')
         
-        # Читання з cookies, якщо немає в GET
         if diagnosis_query is None:
-            diagnosis_query = self.request.COOKIES.get('last_diagnosis_filter')
+            cookie_val = self.request.COOKIES.get('last_diagnosis_filter')
+            if cookie_val:
+                diagnosis_query = urllib.parse.unquote(cookie_val)
 
         if diagnosis_query:
-            queryset = queryset.filter(diagnosis__icontains=diagnosis_query)
+            query_lower = diagnosis_query.lower()
+            matching_ids = [
+                app.id for app in queryset 
+                if app.diagnosis and query_lower in app.diagnosis.lower()
+            ]
+            queryset = queryset.filter(id__in=matching_ids)
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         diagnosis_query = self.request.GET.get('diagnosis')
         if diagnosis_query is None:
-            diagnosis_query = self.request.COOKIES.get('last_diagnosis_filter', '')
+            cookie_val = self.request.COOKIES.get('last_diagnosis_filter')
+            diagnosis_query = urllib.parse.unquote(cookie_val) if cookie_val else ''
         context['current_diagnosis_filter'] = diagnosis_query
         return context
 
@@ -44,7 +52,6 @@ class AppointmentDetailView(DetailView):
 
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
-        # Збереження в сесії
         appointment = self.get_object()
         request.session['last_viewed_appointment'] = {
             'id': appointment.id,
@@ -53,7 +60,6 @@ class AppointmentDetailView(DetailView):
         return response
 
 class AppointmentCreateView(CreateView):
-    # Знято LoginRequiredMixin - доступно для всіх
     model = Appointment
     form_class = AppointmentForm
     template_name = 'appointments/appointment_form.html'
